93b3165695e36d405326ba4d58b89b951c65e98d
e3rosale <eduardo.d.rosales1@gmail.com>
2026-04-27 16:54:40 -0700

    feat: added .gitignore entries and also declared dotenv dependency in pubspec.yaml

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,20 @@
 import 'package:flutter/material.dart';
-// import 'package:supabase_flutter/supabase_flutter.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
 // import 'app.dart';
 // import 'core/constants/app_constants.dart';
 
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  await dotenv.load(fileName: ".env");
+  
+  await Supabase.initialize(
+    url: "somehting",
+    anonKey: "else",
+  );
+
   runApp(const MyApp());
 }
