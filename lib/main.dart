328a24d08e4fbd1f59637896cb5d7124832856b1
e3rosale <eduardo.d.rosales1@gmail.com>
2026-04-27 17:22:48 -0700

    feat: auto sort import statements in main.dart and added .env asset for flutter in pubspec.yaml

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
 // import 'app.dart';
 // import 'core/constants/app_constants.dart';
-
-import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -12,8 +11,8 @@ Future<void> main() async {
   await dotenv.load(fileName: ".env");
   
   await Supabase.initialize(
-    url: "somehting",
-    anonKey: "else",
+    url: dotenv.env['SUPABASE_URL']!,
+    anonKey: dotenv.env['SUPABASE_ANON_KEY']!,
   );
 
   runApp(const MyApp());
