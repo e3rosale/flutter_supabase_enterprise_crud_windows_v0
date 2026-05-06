b06d89d70155f45977326bd52a884dac81d56b83
e3rosale <eduardo.d.rosales1@gmail.com>
2026-05-06 16:51:33 -0700

    feat: added UsersState class for centralized state variables

---
+++ lib/features/users/presentation/viewmodels/users_state.dart
@@ -0,0 +1,30 @@
+// import '../../domain/entities/user_entity.dart';
+
+class UsersState {
+  final List<UserEntity> users;
+  final bool isLoading;
+  final String? errorMessage;
+
+  const UsersState({
+    this.users = const [],
+    this.isLoading = false,
+    this.errorMessage,
+  });
+
+  UsersState copyWith({
+    List<UserEntity>? users,
+    bool? isLoading,
+    String? errorMessage,
+    bool clearError = false,
+  }) {
+    final nextErrorMessage = clearError
+        ? null
+        : errorMessage ?? this.errorMessage;
+
+    return UsersState(
+      users: users ?? this.users,
+      isLoading: isLoading ?? this.isLoading,
+      errorMessage: nextErrorMessage,
+    );
+  }
+}
