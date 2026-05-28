ea8506943a683644051b7ecf3dbbd6a7a56d63e0
e3rosale <eduardo.d.rosales1@gmail.com>
2026-05-27 19:06:18 -0700

    feat(user): implement CreateUser use case

---
+++ lib/features/users/domain/usecases/create_user.dart
@@ -0,0 +1,12 @@
+import 'package:flutter_supabase_enterprise_crud_windows_v0/core/utils/result.dart';
+import 'package:flutter_supabase_enterprise_crud_windows_v0/features/users/domain/repositories/user_repository.dart';
+
+class CreateUser {
+  final UserRepository repository;
+
+  CreateUser(this.repository);
+
+  Future<Result<void>> call({required String name, required String email}) {
+    return repository.createUsers(name: name, email: email);
+  }
+}
